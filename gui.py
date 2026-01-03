555790cafc70ae66338091df19bef5c8455a29d6
Frank <astero2gk@gmail.com>
2026-01-03 15:38:14 +0000

    create basic GUI without interaction, 20260103

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a TodoList")
 input_box = sg.InputText(tooltip="Enter TodoList")
 add_button = sg.Button("Add")
-
+print_button = sg.Button("Print")
 window = sg.Window("TodoList", [[label], [input_box,add_button]])
 window.read()
 window.close()
