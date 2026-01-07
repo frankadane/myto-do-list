a6624bcfcda89fd2a61093168a5996b965c70440
Frank <astero2gk@gmail.com>
2026-01-07 20:25:56 +0000

    Implement the Edit feature

--- gui.py
+++ gui.py
@@ -1,14 +1,22 @@
+from FreeSimpleGUI.elements import list_box
+
 import functions
 import FreeSimpleGUI as sg
 
-label = sg.Text("Type in a TodoList")
+label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter TodoList",
                          key="todo")
 add_button = sg.Button("Add")
-print_button = sg.Button("Print")
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key="todos",
+                      enable_events=True,
+                      size=(45,15))
+edit_button = sg.Button("Edit")
+
 window = sg.Window("TodoList", [[label],
-                                [input_box,add_button]],
-                   font=("Helvetica", 12))
+                                [input_box,add_button],
+                                [list_box,edit_button]],
+                   font=("Helvetica", 15))
 
 while True:
     event, values = window.read()
@@ -19,6 +27,18 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
